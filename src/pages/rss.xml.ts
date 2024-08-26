4b5171d64bb49c635ad8152aaa3bc28038545583
jlonuz <lonuz.j@gmail.com>
2024-08-25 22:00:39 -0400

    adding styles to rss feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,10 @@
 import type { APIRoute } from 'astro';
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import sanitizeHtml from'sanitize-html';
+import MarkdownIt from 'markdown-it';
+
+const parser = new MarkdownIt();
 
 export const GET: APIRoute =async ({ params, request, site }) => {
 const blogPosts = await getCollection('blog')
@@ -11,16 +15,30 @@ const blogPosts = await getCollection('blog')
         title: 'Josefina Blog',
         // `<description>` field in output xml
         description: 'Un simple blog sobre mis aventuras con Astro',
+        xmlns: {
+            media: 'http://search.yahoo.com/mrss/',
+          },
         // Pull in your project "site" from the endpoint context
         // https://docs.astro.build/en/reference/api-reference/#contextsite
         site: site ?? '',
         // Array of `<item>`s in output xml
         // See "Generating items" section for examples using content collections and glob imports
-        items: blogPosts.map( ({ data, slug }) => ({
+        items: blogPosts.map( ({ data, slug, body }) => ({
             title: data.title,
             pubDate: data.date,
             description: data.description,
             link: `/blog/${slug}`,
+            content: sanitizeHtml(parser.render(body), {
+                allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+              }),
+
+              customData: `<media:content
+                  type="image/${data.image.format === 'jpg' ? 'jpeg' : 'png'}"
+                  width="${data.image.width}"
+                  height="${data.image.height}"
+                  medium="image"
+                  url="${site + data.image.src}" />
+              `,
         })),
         // (optional) inject custom xml
         customData: `<language>es-mx</language>`,
